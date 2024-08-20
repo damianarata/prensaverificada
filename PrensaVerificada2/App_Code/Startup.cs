b239dc84a03ceada21de4982d8227477c9a19cb7
Damian Rodriguez Arata <rodriguezarata.d@gmail.com>
2024-08-20 19:52:00 -0300

    Adding .net proj

---
+++ PrensaVerificada2/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PrensaVerificada2.Startup))]
+namespace PrensaVerificada2
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
