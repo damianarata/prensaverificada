9695e4f546c7d4addfe6239cf802e4ec73d4c65c
Damian Arata <rodriguezarata.d@gmail.com>
2024-09-17 17:18:39 -0300

    Adding session to pages

--- PrensaVerificada2/Assets/Index.aspx.cs
+++ PrensaVerificada2/Assets/Index.aspx.cs
@@ -11,7 +11,10 @@ namespace PrensaVerificada2.Assets
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (Session["usuario"] == null)
+            {
+                Response.Redirect("Login.aspx");
+            }
         }
     }
 }
